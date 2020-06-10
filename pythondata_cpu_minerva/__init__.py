cf29b534aae5466d4d079b92260b7d2fe6f400c3
LiteX <robot@timvideos.us>
2020-06-10 18:22:15 +0000

    Updating pythondata-cpu-minerva to 0.0.post154
    Updated data to v0.0-96-g53251ba based on 53251badb3fe8fae45e30b7e64c38489dde08af9 from http://github.com/lambdaconcept/minerva.
    > commit 53251badb3fe8fae45e30b7e64c38489dde08af9
    > Author: Jean-François Nguyen <jf@lambdaconcept.com>
    > Date:   Tue May 5 20:01:26 2020 +0200
    >
    >     debug: unbreak the debug unit.
    >
    >     * fix a logic loop between `exception.m_ebreak` and `trigger.trap`
    >     * correctly add DCSR and DPC to the register file
    >     * fix some timing issues
    >
    >     Fixes #8.
    >
    
    Updated using 0.0.post58 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_minerva/__init__.py
+++ pythondata_cpu_minerva/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "sources")
 src = "http://github.com/lambdaconcept/minerva"
 
 # Module version
-version_str = "0.0.post149"
-version_tuple = (0, 0, 149)
+version_str = "0.0.post154"
+version_tuple = (0, 0, 154)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post149")
+    pversion = V("0.0.post154")
 except ImportError:
     pass
 
@@ -38,11 +38,11 @@ Date:   Tue May 5 20:01:26 2020 +0200
 """
 
 # Tool version info
-tool_version_str = "0.0.post53"
-tool_version_tuple = (0, 0, 53)
+tool_version_str = "0.0.post58"
+tool_version_tuple = (0, 0, 58)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post53")
+    ptool_version = V("0.0.post58")
 except ImportError:
     pass
 
