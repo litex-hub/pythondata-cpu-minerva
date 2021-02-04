9bdd9410704e47f4ec66fd2be7e377f5c9a49bc1
LiteX Robot <robot@timvideos.us>
2021-02-04 02:38:59 +0000

    Updating pythondata-cpu-minerva to 0.0.post172
    Updated data to v0.0-99-g0b5f6b2 based on 0b5f6b2466367f262f9a16a83f9c86fc7f008edf from https://github.com/lambdaconcept/minerva.
    > commit 0b5f6b2466367f262f9a16a83f9c86fc7f008edf
    > Author: Jean-François Nguyen <jf@lambdaconcept.com>
    > Date:   Fri Jan 22 15:20:35 2021 +0100
    >
    >     fetch: workaround YosysHQ/yosys#2035.
    >
    
    Updated using 0.0.post73 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_minerva/__init__.py
+++ pythondata_cpu_minerva/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "sources")
 src = "https://github.com/lambdaconcept/minerva"
 
 # Module version
-version_str = "0.0.post171"
-version_tuple = (0, 0, 171)
+version_str = "0.0.post172"
+version_tuple = (0, 0, 172)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post171")
+    pversion = V("0.0.post172")
 except ImportError:
     pass
 
@@ -32,11 +32,11 @@ Date:   Fri Jan 22 15:20:35 2021 +0100
 """
 
 # Tool version info
-tool_version_str = "0.0.post72"
-tool_version_tuple = (0, 0, 72)
+tool_version_str = "0.0.post73"
+tool_version_tuple = (0, 0, 73)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post72")
+    ptool_version = V("0.0.post73")
 except ImportError:
     pass
 
