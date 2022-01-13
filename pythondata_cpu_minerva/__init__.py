110143b0495c674ced4ebefcaf5880b77649680a
LiteX Robot <robot@timvideos.us>
2022-01-13 18:33:46 +0000

    Updating pythondata-cpu-minerva to 0.0.post233
    Updated data to v0.0-112-g3cb469f based on 3cb469f8fe451838ee2105842d8d4695a64d234c from https://github.com/lambdaconcept/minerva.
    > commit 3cb469f8fe451838ee2105842d8d4695a64d234c
    > Author: Catherine <whitequark@whitequark.org>
    > Date:   Sun Dec 12 15:39:03 2021 +0000
    >
    >     nMigen has been renamed to Amaranth HDL.
    >
    
    Updated using 0.0.post121 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_minerva/__init__.py
+++ pythondata_cpu_minerva/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "sources")
 src = "https://github.com/lambdaconcept/minerva"
 
 # Module version
-version_str = "0.0.post232"
-version_tuple = (0, 0, 232)
+version_str = "0.0.post233"
+version_tuple = (0, 0, 233)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post232")
+    pversion = V("0.0.post233")
 except ImportError:
     pass
 
@@ -32,11 +32,11 @@ Date:   Sun Dec 12 15:39:03 2021 +0000
 """
 
 # Tool version info
-tool_version_str = "0.0.post120"
-tool_version_tuple = (0, 0, 120)
+tool_version_str = "0.0.post121"
+tool_version_tuple = (0, 0, 121)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post120")
+    ptool_version = V("0.0.post121")
 except ImportError:
     pass
 
