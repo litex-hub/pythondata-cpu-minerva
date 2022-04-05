67388eea88943d8c029fb5107ae2dc7487f358ab
LiteX Robot <robot@timvideos.us>
2022-04-05 13:37:12 +0000

    Updating pythondata-cpu-minerva to 0.0.post246
    Updated data to v0.0-120-g08251da based on 08251daae42ec8cfc54fb82865a5942727186192 from https://github.com/lambdaconcept/minerva.
    > commit 08251daae42ec8cfc54fb82865a5942727186192
    > Author: Jean-François Nguyen <jf@jfng.fr>
    > Date:   Tue Apr 5 15:33:21 2022 +0200
    >
    >     stage: fix commit 6c3294b9.
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_minerva/__init__.py
+++ pythondata_cpu_minerva/__init__.py
@@ -4,30 +4,30 @@ data_location = os.path.join(__dir__, "sources")
 src = "https://github.com/lambdaconcept/minerva"
 
 # Module version
-version_str = "0.0.post241"
-version_tuple = (0, 0, 241)
+version_str = "0.0.post246"
+version_tuple = (0, 0, 246)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post241")
+    pversion = V("0.0.post246")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post115"
-data_version_tuple = (0, 0, 115)
+data_version_str = "0.0.post120"
+data_version_tuple = (0, 0, 120)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post115")
+    pdata_version = V("0.0.post120")
 except ImportError:
     pass
-data_git_hash = "d4a55f23855f43c9a13fa2f7c0f7c1a62242826b"
-data_git_describe = "v0.0-115-gd4a55f2"
+data_git_hash = "08251daae42ec8cfc54fb82865a5942727186192"
+data_git_describe = "v0.0-120-g08251da"
 data_git_msg = """\
-commit d4a55f23855f43c9a13fa2f7c0f7c1a62242826b
-Author: Tobias Müller <Tobias_Mueller@twam.info>
-Date:   Sun Jan 16 11:39:10 2022 +0100
+commit 08251daae42ec8cfc54fb82865a5942727186192
+Author: Jean-François Nguyen <jf@jfng.fr>
+Date:   Tue Apr 5 15:33:21 2022 +0200
 
-    Fix nmigen include in cli.py
+    stage: fix commit 6c3294b9.
 
 """
 
